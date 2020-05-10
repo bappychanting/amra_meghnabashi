104b70e688dcc993fd7716dbca270157bea3dffe
sterijuanna <bappychanting@outlook.com>
2020-05-10 23:56:43 +0600

    admin web contents done

---
+++ resources/views/layouts/master.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Header -->
+<head>
+
+	<meta charset="UTF-8">
+	<?php startblock('meta_tags') ?><?php endblock() ?>
+
+	<!-- Favicon-->
+	<?php echo icon('img/favicon.png'); ?>
+
+	<title><?php startblock('title') ?><?php endblock() ?></title>
+
+	<!-- Font Awesome -->
+	<?php echo style('plugins/fontawesome/css/all.min.css'); ?>
+	<!-- Google Fonts Roboto -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+	<!-- Bootstrap core CSS -->
+	<?php echo style('css/bootstrap.min.css'); ?>
+	<!-- Material Design Bootstrap -->
+	<?php echo style('css/mdb.min.css'); ?>
+	<!-- Custom styles (optional) -->
+	<?php echo style('css/style.css'); ?>
+
+</head>
+<!-- #ENDS# Header -->
+
+<body>
+
+	<!-- Navigation -->
+	<?php append('layouts.navigation'); ?>
+
+	<!-- Page Content -->
+	<div class="container">
+		<?php startblock('content') ?><?php endblock() ?>
+	</div>
+
+	<!-- Footer -->
+	<?php append('layouts.footer'); ?>
+
+	<!-- jQuery -->
+	<?php echo script('js/jquery.min.js'); ?>
+	<!-- Bootstrap tooltips -->
+	<?php echo script('js/popper.min.js'); ?>
+	<!-- Bootstrap core JavaScript -->
+	<?php echo script('js/bootstrap.min.js'); ?>
+	<!-- MDB core JavaScript -->
+	<?php echo script('js/mdb.min.js'); ?>
+	<!-- Custom JavaScript -->
+	<?php echo script('js/script.js'); ?>
+
+</body>
+
+</html>
--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -1,67 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php inherits('layouts.master'); ?>
 
-  <!-- Header -->
-  <head>
-
-    <meta charset="UTF-8">
-    <meta name="description" content="CodeCube Framework">
-    <meta name="keywords" content="PHP,HTML,CSS,XML,JavaScript">
-    <meta name="author" content="Mahadi Hasan">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Favicon-->
-    <?php echo icon('img/favicon.png'); ?>
-
-    <title><?php echo 'Welcome || '.title(); ?></title>
+<?php startblock('title') ?>
     
-    <!-- CSS-->
-    <?php echo style('plugins/bootstrap/css/bootstrap.min.css'); ?>
+    <?php echo title(); ?>
 
-    <style>
-      body {
-          background-color: #f2f2f2;
-      }
-      a:link {
-          text-decoration: none;
-      }
-      .brand {  
-        position:absolute;
-        bottom:0px;
-        right:25%;
-        left:50%;
-      }
-      .framework_icon{
-        height: 60px;
-      }
-    </style>
+<?php endblock() ?>
 
-  </head>
-  <!-- #ENDS# Header -->
+<?php startblock('content') ?>
 
-  <body>
-    
-    <div class="text-center">
-      <h1 class="my-5 text-secondary">
-        <?php echo image('resources/assets/img/logo.png', 'framework_icon', ['class'=>'framework_icon pr-3']); ?>CodeCube
-      </h1>
-      <p class="my-5">
-        <a href="https://www.codecubeit.com/" class="mx-5">Documentation</a>
-        <a href="<?php echo route('admin/signin'); ?>" class="mx-5">Demo App</a>
-        <a href="/database_migration" class="mx-5">Migration</a>
-        <a href="https://www.codecubeit.com/know-us/" class="mx-5">About Us</a>
-      </p> 
-    </div> 
+<p>hello</p>
 
-    <p class="small brand">
-      <a href="https://www.codecubeit.com/" class="text-muted">codecube.com</a> 
-    </p> 
-
-    <!-- Bootstrap tooltips -->
-    <?php echo script('plugins/bootstrap/js/popper.min.js'); ?>
-    <!-- Bootstrap core JavaScript -->
-    <?php echo script('plugins/bootstrap/js/bootstrap.min.js'); ?>
-
-  </body>
-
-</html>
+<?php endblock() ?>
