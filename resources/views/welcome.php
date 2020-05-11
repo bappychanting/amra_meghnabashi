5a96d8413314d01150b70ff9821a7705ad7a1ab5
sterijuanna <bappychanting@outlook.com>
2020-05-11 18:09:02 +0600

    added pages

--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -9,7 +9,17 @@
 <?php startblock('content') ?>
 
 <div class="front-page-top-div">
-    div with background
+	<div class="container text-white py-5">
+    	<h6>
+    		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    	</h6>
+	</div>
+</div>
+
+<div class="container my-3 py-3">
+	<h6>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</h6>
 </div>
 
 <?php endblock() ?>
