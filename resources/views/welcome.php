ac51c16adf6ced2b00730d38898ce58cef8eecd9
sterijuanna <bappychanting@outlook.com>
2020-05-12 10:48:00 +0600

    updated welcome page

--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -41,7 +41,7 @@
 </section>
 <!-- #END# About Us -->
 
-<!--Section: Not enough-->
+<!--Our Goals-->
 <section class="container py-3">
 <h2 class="green-text font-weight-bold text-center my-3"><?php echo locale('views', 'our_goals'); ?></h2>
 <div class="row my-5">
@@ -98,59 +98,11 @@
 </div>
 <hr>
 </section>
-<!--Section: Not enough-->
+<!-- #END# Our Goals -->
 
-<!-- Dynamic -->
-<section class="container py-3">
-	<h2 class="green-text font-weight-bold my-3 text-center"><i class="fas fa-users pr-2"></i><?php echo locale('views', 'our_members'); ?></h2>
-	<div class="row">
-	<?php $count = 1; foreach ($members['members'] as $member) { ?>
-	<div class="col-sm-2">
-		<img src="<?php echo $member['image_path']; ?>" class="rounded-circle mx-2 mt-3" alt="<?php echo $member['name']; ?>" height="100px" width="100px">
-	</div>
-	<?php if($count == 6){ break; } $count++; } ?>
-	</div>
-	<div class="row justify-content-center mt-3 mb-5">
-		<a class="btn btn-danger" href="<?php echo route('members'); ?>"><i class="fas fa-external-link-alt pr-2"></i><?php echo locale('views', 'see_all_members'); ?></a>
-	</div>
-	<h2 class="green-text font-weight-bold my-3 text-center"><i class="fas fa-briefcase pr-2"></i><?php echo locale('views', 'our_projects'); ?></h2>
-	<div class="row">
-		<?php $count = 1; foreach ($projects['projects'] as $project) { ?>
-		<div class="col-sm-3">
-			<div class="border border-light px-3 py-4 mx-3" style='height:150px;'>
-				<h5 class="green-text"><?php echo $project['name'] ?></h5><hr>
-				<p class="grey-text"><?php echo strip_tags(substr($project['details'], 0, 30)); ?>...</p>
-			</div>
-		</div>
-		<?php if($count == 4){ break; } $count++; } ?>
-	</div>
-	<div class="row justify-content-center mt-3 mb-5">
-		<a class="btn btn-danger" href="<?php echo route('projects'); ?>"><i class="fas fa-external-link-alt pr-2"></i><?php echo locale('views', 'see_all_projects'); ?></a>
-	</div>
-	<h2 class="green-text font-weight-bold my-3 text-center"><i class="fas fa-newspaper pr-2"></i><?php echo locale('views', 'news'); ?></h2>
-	<div class="row">
-	<?php $count = 1; foreach ($newses['news'] as $news) { ?>
-		<div class="col-sm-4">
-			<div class="card">
-				<img class="card-img-top" src="<?php echo $news['image_path'] ?>" alt="<?php echo $news['title'] ?>" height='300px'>
-				<div class="card-body">
-					<h4 class="card-title"><?php echo strip_tags(substr($news['title'], 0, 30)); ?></h4>
-					<p class="card-text"><?php echo strip_tags(substr($news['details'], 0, 80)); ?>...</p>
-				</div>
-			</div>
-		</div>
-		<?php if($count == 3){ break; } $count++; } ?>
-	</div>
-	<div class="row justify-content-center mt-3 mb-5">
-		<a class="btn btn-danger" href="<?php echo route('news'); ?>"><i class="fas fa-external-link-alt pr-2"></i><?php echo locale('views', 'see_all_news'); ?></a>
-	</div>
-	<hr>
-</section>
-<!-- #END# Dynamic -->
-
-<!-- More -->
+<!-- Our Achievements -->
 <section class='container py-3'>
-	<h2 class="green-text font-weight-bold my-3 text-center"><?php echo locale('views', 'extra_section_title'); ?></h2>
+	<h2 class="green-text font-weight-bold my-3 text-center"><?php echo locale('views', 'our_achievements'); ?></h2>
 	<div class="row features-small mt-5 wow fadeIn">
 	  <div class="col-xl-3 col-lg-6">
 	    <div class="row">
@@ -250,7 +202,55 @@
 	    </div>
 	  </div>
 	</div>
+	<hr>
 </section>
-<!-- #END# More -->
+<!-- #END# Our Achievements -->
+
+<!-- Dynamic -->
+<section class="container py-3">
+	<h2 class="green-text font-weight-bold my-3 text-center"><i class="fas fa-users pr-2"></i><?php echo locale('views', 'our_members'); ?></h2>
+	<div class="row">
+	<?php $count = 1; foreach ($members['members'] as $member) { ?>
+	<div class="col-sm-2">
+		<img src="<?php echo $member['image_path']; ?>" class="rounded-circle mx-2 mt-3" alt="<?php echo $member['name']; ?>" height="100px" width="100px">
+	</div>
+	<?php if($count == 6){ break; } $count++; } ?>
+	</div>
+	<div class="row justify-content-center mt-3 mb-5">
+		<a class="btn btn-danger" href="<?php echo route('members'); ?>"><i class="fas fa-external-link-alt pr-2"></i><?php echo locale('views', 'see_all_members'); ?></a>
+	</div>
+	<h2 class="green-text font-weight-bold my-3 text-center"><i class="fas fa-briefcase pr-2"></i><?php echo locale('views', 'our_projects'); ?></h2>
+	<div class="row">
+		<?php $count = 1; foreach ($projects['projects'] as $project) { ?>
+		<div class="col-sm-3">
+			<div class="border border-light px-3 py-4 mx-3" style='height:150px;'>
+				<h5 class="green-text"><?php echo $project['name'] ?></h5><hr>
+				<p class="grey-text"><?php echo strip_tags(substr($project['details'], 0, 30)); ?>...</p>
+			</div>
+		</div>
+		<?php if($count == 4){ break; } $count++; } ?>
+	</div>
+	<div class="row justify-content-center mt-3 mb-5">
+		<a class="btn btn-danger" href="<?php echo route('projects'); ?>"><i class="fas fa-external-link-alt pr-2"></i><?php echo locale('views', 'see_all_projects'); ?></a>
+	</div>
+	<h2 class="green-text font-weight-bold my-3 text-center"><i class="fas fa-newspaper pr-2"></i><?php echo locale('views', 'news'); ?></h2>
+	<div class="row">
+	<?php $count = 1; foreach ($newses['news'] as $news) { ?>
+		<div class="col-sm-4">
+			<div class="card">
+				<img class="card-img-top" src="<?php echo $news['image_path'] ?>" alt="<?php echo $news['title'] ?>" height='300px'>
+				<div class="card-body">
+					<h4 class="card-title"><?php echo strip_tags(substr($news['title'], 0, 30)); ?></h4>
+					<p class="card-text"><?php echo strip_tags(substr($news['details'], 0, 80)); ?>...</p>
+				</div>
+			</div>
+		</div>
+		<?php if($count == 3){ break; } $count++; } ?>
+	</div>
+	<div class="row justify-content-center mt-3 mb-5">
+		<a class="btn btn-danger" href="<?php echo route('news'); ?>"><i class="fas fa-external-link-alt pr-2"></i><?php echo locale('views', 'see_all_news'); ?></a>
+	</div>
+</section>
+<!-- #END# Dynamic -->
 
 <?php endblock() ?>
