d088673e8088ede7884603d19f5acf569096520d
sterijuanna <bappychanting@outlook.com>
2020-05-18 06:52:37 +0600

    added official logo

--- resources/views/layouts/topbar.php
+++ resources/views/layouts/topbar.php
@@ -19,8 +19,8 @@
 </div>
 
 <nav class="navbar navbar-expand-lg navbar-dark green darken-4">
-  <div class="container py-2">
-    <a class="navbar-brand font-weight-bold" href="<?php echo route('welcome'); ?>"><i class="fas fa-home red-text pr-2"></i><?php echo locale('views', 'app_title'); ?></a>
+  <div class="container">
+    <a class="navbar-brand font-weight-bold" href="<?php echo route('welcome'); ?>"><?php echo image('resources/assets/img/favicon.png', APP_NAME, ['class'=>'z-depth-0 img-fluid pr-2', 'style'=>'width:40px;height:40px;']); echo locale('views', 'app_title'); ?></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
     <div class="collapse navbar-collapse" id="basicExampleNav">
       <ul class="navbar-nav ml-auto">
