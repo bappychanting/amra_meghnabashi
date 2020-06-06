00bc827b02ff5a000e3442676b8bc01c8180e729
sterijuanna <bappychanting@outlook.com>
2020-06-06 11:10:11 +0600

    removed about meghna

--- resources/locale/bd/views.php
+++ resources/locale/bd/views.php
@@ -5,9 +5,6 @@ return [
     	// key texts
 	'app_title' => 'আমরা মেঘনাবাসী',
 	'links' => 'লিংকসমূহ',
-    'members' => 'সদস্যগণ',
-    'projects' => 'প্রকল্পসমূহ',
-    'news' => 'খবর',
     'donate_now' => 'দান করুন!',
     'who_are_we' => 'আমাদের পরিচয়',
     'our_programs' => 'আমাদের কর্মসূচি',
@@ -16,6 +13,16 @@ return [
     'beneficiaries' => 'দানগ্রাহী',
     'reached' => 'পৌঁছানো',
     'job_scholarship' => 'চাকরী ও বৃত্তি',
+    'show_details' => 'বিস্তারিত দেখুন',
+    'read_more' => 'আরও পড়ুন',
+    'go_back' => 'ফিরে যান',
+    'subscribe_us' => 'আমাদের সাবস্ক্রাইব করুন',
+    'submit' => 'জমা দিন',
+    'close' => 'বন্ধ করুন',
+    'contact_us' => 'যোগাযোগ করুন',
+    'follow_us' => 'আমাদের অনুসরণ করুন',
+
+        // Our Meghna
     'our_meghna' => 'আমাদের মেঘনা',
     'about_meghna' => 'একনজরে মেঘনা',
     'meghna_bridge' => 'ব্রীজ কালভার্ট',
@@ -33,6 +40,9 @@ return [
     'meghna_fighter' => 'মুক্তিযুদ্ধা',
     'meghna_personality' => 'মেঘনার বিশিষ্টজন',
     'search_something' => 'অনুসন্ধান করুন',
+
+        // Members
+    'members' => 'সদস্যগণ',
     'our_members' => 'আমাদের সদস্যগণ',
     'see_all_members' => 'সমস্ত সদস্যদের দেখুন',
     'volunteers' => 'সেচ্ছাসেবকবৃন্দ',
@@ -41,16 +51,15 @@ return [
     'vips' => 'সুধীমন্ডলী',
     'advisers' => 'উপদেষ্টামন্ডলী',
     'be_volunteer' => 'স্বেচ্ছাসেবক হোন',
+
+        // News    
+    'news' => 'খবর',
+    'see_all_news' => 'সমস্ত খবর দেখুন',
+    'upcoming_events' => 'আসন্ন ইভেন্টস',
+
+        // Projects
+    'projects' => 'প্রকল্পসমূহ',
     'our_projects' => 'আমাদের প্রকল্পসমূহ',
     'see_all_projects' => 'সমস্ত প্রকল্পসমূহ দেখুন',
-    'see_all_news' => 'সমস্ত খবর দেখুন',
-    'show_details' => 'বিস্তারিত দেখুন',
-    'read_more' => 'আরও পড়ুন',
-    'go_back' => 'ফিরে যান',
-    'subscribe_us' => 'আমাদের সাবস্ক্রাইব করুন',
-    'submit' => 'জমা দিন',
-    'close' => 'বন্ধ করুন',
-    'contact_us' => 'যোগাযোগ করুন',
-    'follow_us' => 'আমাদের অনুসরণ করুন',
 
 ];
